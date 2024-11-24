60287c322ccb8ab688387a066679390b909dea56
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-11-24 20:40:56 +0100

    added timer

--- my-react-app/src/pages/timer.jsx
+++ my-react-app/src/pages/timer.jsx
@@ -1,18 +1,35 @@
 import "./page.css";
+import {useState} from "react";
 function Timer(){
     let count = 0;
     const handelEvent = (name) => {
         if (count < 3) {
             count++
-            console.log(`${name} you clicked me ${count} times`)
         } else {
-            console.log (`${name} stop clicking me`)
         }
+    }
+   const [number, setNumber] = useState(0);
+    const increseNumber = () => {
+     setNumber(number + 1);
+    }
+     const resetNumber = () => {
+        setNumber(number === 0);
+     }
+        const decreseNumber = () => {
+            setNumber(number - 1);
     }
 
     return(
-        <div>
-            <button classname="counter-button" cnClick={() => handelEvent("")}>
+
+        <div class ="timer">
+            <h1 class ="number">current count: {number} </h1>
+            <button class="buttons3" onClick={decreseNumber}>
+                count down
+            </button>
+            <button class="buttons2" onClick={resetNumber}>
+                reset
+            </button>
+            <button class="buttons" onClick={increseNumber}>
                 count up
             </button>
         </div>
