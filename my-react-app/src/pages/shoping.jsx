cec656e64af91651553db157437d26001221b1c7
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-12-02 19:50:46 +0100

    added

--- my-react-app/src/pages/shoping.jsx
+++ my-react-app/src/pages/shoping.jsx
@@ -4,40 +4,43 @@ import { useState } from "react";
 function shoping() {
     const [Poster, setPoster] = useState(["wolfposter", "leopardposter", "foxposter", "huskyposter", "catposter"]);
 
-    function handleAddPoster() {
-        const newPoster = document.getElementById("posteroptions").value;
-        document.getElementById("posteroptions").value = "";
+    function handleAddPoster(index) {
+        const newPoster = document.getElementById("posterOptions").value;
+        document.getElementById("posterOptions").value = "";
 
-        setPoster((Poster) => [...Poster, newPoster]);
+        setPoster((P) => [...P,newPoster]);
     }
-    function handleRemovePoster() {
+    function handleRemovePoster(index) {
         setPoster(Poster.filter((_, i) => i !== index));
     }
+
+
+function handleAmountOf() {
+        let count = 0;
+        const handleEvent = (number) => {
+            if (count < 3) {
+                count++
+            } else {
+            }
+        }}
+        const [number, setNumber] = useState(0);
+        const increseNumber = () => {
+            setNumber(number + 1);
+        }
+        const decreseNumber = () => {
+            setNumber(number - 1);
+        }
     return (
         <div class="shopingSection">
             <h2 class="Header"> list of posters</h2>
             <ul class="options">
                 {Poster.map((Poster, index) => (
-                <li key={index} onClick={() => handleRemovePoster(index)}>
-                    {Poster}</li>
-            ))}
+                    <li key={index} onClick={() => handleRemovePoster(index)}>{Poster}</li>
+                ))}
             </ul>
-            <input type="text"  class="textzone" placeholder="enter poster animal" id="posteroption" />
-            <button class="add" onClick={handleAddPoster}>add poster</button><button class="amount">add amount</button><button  class="remove" onClick={handleRemovePoster}>remove poster</button>
-            <input type="text"  class="textzone" placeholder="enter poster animal" id="posteroption" />
-            <button class="add" onClick={handleAddPoster}>add poster</button><button class="amount">add amount</button><button  class="remove" onClick={handleRemovePoster}>remove poster</button>
-            <input type="text"  class="textzone" placeholder="enter poster animal" id="posteroption" />
-            <button class="add" onClick={handleAddPoster}>add poster</button><button class="amount">add amount</button><button  class="remove" onClick={handleRemovePoster}>remove poster</button>
-            <input type="text"  class="textzone" placeholder="enter poster animal" id="posteroption" />
-            <button class="add" onClick={handleAddPoster}>add poster</button><button class="amount">add amount</button><button  class="remove" onClick={handleRemovePoster}>remove poster</button>
-            <input type="text"  class="textzone" placeholder="enter poster animal" id="posteroption" />
-            <button class="add" onClick={handleAddPoster}>add poster</button><button class="amount">add amount</button><button  class="remove" onClick={handleRemovePoster}>remove poster</button>
-            <input type="text"  class="textzone" placeholder="enter poster animal" id="posteroption" />
-            <button class="add" onClick={handleAddPoster}>add poster</button><button class="amount">add amount</button><button  class="remove" onClick={handleRemovePoster}>remove poster</button>
-
+            <input type="text" class="textzone" placeholder="enter poster animal" id="posterOption" />
+            <button class="add" onClick={() => handleAddPoster(index)}>add poster</button><button class="minus" onClick={decreseNumber}>-</button><button class="amount" onClick={handleAmountOf}>amount: {number}</button><button class="plus" onClick={increseNumber}>+</button><button class="remove" onClick={() => handleRemovePoster(index)}>remove poster</button>
         </div>
-    )
-
-
+    );
 }
 export default shoping;
